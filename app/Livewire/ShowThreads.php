7829fdd5d351eb2a34b69f3ce25fd7f758e705a4
hviveros <humberto.viverosrios@gmail.com>
2024-01-18 22:12:58 -0300

    Filtrado por Categorías

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -9,6 +9,13 @@ use Livewire\Component;
 class ShowThreads extends Component
 {
     public $search = '';
+    public $category = '';
+
+    // Una función propia que se activará con un clic
+    public function filterByCategory($category)
+    {
+        $this->category = $category;
+    }
 
     public function render()
     {
@@ -21,6 +28,14 @@ class ShowThreads extends Component
         // Con el buscador, se debe segmentar la consulta, se trabaja con query() y a partir de allí se realizan las configuraciones y condiciones
         $threads = Thread::query();
         $threads->where('title', 'like', "%$this->search%");
+
+        // Filtrado por categorías
+        // Si hay algo en la variable $category, se alterará la consulta $threads
+        // sería agregar un nuevo where a la consulta
+        if ($this->category) {
+            $threads->where('category_id', $this->category);
+        }
+
         $threads->withCount('replies');
         $threads->latest();
 
--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -5,7 +5,7 @@
         <ul>
             @foreach($categories as $category)
             <li class="mb-2">
-                <a href="" class="p-2 rounded flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                <a href="#" wire:click.prevent="filterByCategory('{{ $category->id }}')" class="p-2 rounded flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
                     <span class="w-2 h-2 rounded-full" style="background-color: {{ $category->color }}"></span>
                     {{ $category->name }}
                 </a>
@@ -13,7 +13,7 @@
             @endforeach
 
             <li class="mb-2">
-                <a href="" class="p-2 rounded flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                <a href="#" wire:click.prevent="filterByCategory('')" class="p-2 rounded flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
                     <span class="w-2 h-2 rounded-full" style="background-color: #000"></span>
                     Todos los resultados
                 </a>
