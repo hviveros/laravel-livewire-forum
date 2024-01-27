8d04c8e61a9c2f9f34df72251b48db4353c7fadb
hviveros <humberto.viverosrios@gmail.com>
2024-01-26 22:04:40 -0300

    Mostrar las respuestas hijas

--- resources/views/livewire/show-reply.blade.php
+++ resources/views/livewire/show-reply.blade.php
@@ -1,23 +1,32 @@
-<div class="rounded-md bg-gradient-to-r from-slate-800 to slate-900 hover:to-slate-800 mb-4">
-    <div class="p-4 flex gap-4">
-        <div>
-            {{-- Se trabaja con la pregunta, que pertenece a un usuario y a este se le aplica el método avatar() --}}
-            <img src="{{ $reply->user->avatar() }}" alt="{{ $reply->user->name }}" class="rounded-md">
-        </div>
-        <div class="w-full">
-            <p class="mb-2 text-blue-600 font-semibold text-xs">
-                {{ $reply->user->name }}
-            </p>
-            <p class="text-white/60 text-xs">
-                {{ $reply->body }}
-            </p>
-            <p class="mt-4 text-white/60 text-xs gap-2 justify-end">
-                <a href="" class="hover:text-white">Responder</a>
-                <a href="" class="hover:text-white">Editar</a>
-            </p>
+<div>
+    <div class="rounded-md bg-gradient-to-r from-slate-800 to slate-900 hover:to-slate-800 mb-4">
+        <div class="p-4 flex gap-4">
+            <div>
+                {{-- Se trabaja con la pregunta, que pertenece a un usuario y a este se le aplica el método avatar() --}}
+                <img src="{{ $reply->user->avatar() }}" alt="{{ $reply->user->name }}" class="rounded-md">
+            </div>
+            <div class="w-full">
+                <p class="mb-2 text-blue-600 font-semibold text-xs">
+                    {{ $reply->user->name }}
+                </p>
+                <p class="text-white/60 text-xs">
+                    {{ $reply->body }}
+                </p>
+                <p class="mt-4 text-white/60 text-xs gap-2 justify-end">
+                    <a href="" class="hover:text-white">Responder</a>
+                    <a href="" class="hover:text-white">Editar</a>
+                </p>
+            </div>
         </div>
     </div>
+
+    {{-- Respuestas hijas --}}
+    {{-- Cómo de aquí es llamado otro componente --}}
+    @foreach ($reply->replies as $item)
+        <div class="ml-8">
+            @livewire('show-reply', ['reply' => $item], key('reply-'.$item->id))
+        </div>
+    @endforeach
 </div>
 
 {{-- Do your work, then step back. --}}
-
